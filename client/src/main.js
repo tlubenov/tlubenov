ded7c20051bfce5cd47b3584dcf2e4169cb270dd
Todor Lubenov <tlubenov@gmail.com>
2021-07-16 14:47:02 +0300

    add toolsets to main and template

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,9 @@ import './registerServiceWorker';
 import router from './router';
 import store from './store';
 
+import './assets/ace.min.css';
+import './assets/style.css';
+
 Vue.config.productionTip = false;
 
 new Vue({
--- client/src/views/Toolset.vue
+++ client/src/views/Toolset.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="toolset">
-    <h1>This is an Toolset page</h1>
+  <div class="clearfix mb4 xs-hide">
+  <!--<div>Categories <p v-for="cat of technology.tech" v-bind:key="cat">{{ cat.category }}</p>
+  </div>-->
+    <div class="sm-col sm-col-3 md-col-2 px2 py3" v-for="item of technology.tech" v-bind:key="item">
+      <a
+        v-bind:href="item.uri"
+        class="h-60 flex flex-column justify-center"
+        target="_blank">
+        <img
+          v-bind:src="item.icon"
+          class="fit block mx-auto grayscale opacity-50 opacity-100-hover grayscale-0-hover"
+          v-bind:alt="item.description"/>
+      </a>
+    </div>
   </div>
 </template>
+
+<script>
+import technology from '../data/tech';
+
+console.log(technology);
+
+export default {
+  name: 'Technologies',
+  data() {
+    return {
+      technology,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
